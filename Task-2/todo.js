c89ca30583cbb85d763a6ce1d7e0ead7e07b5698
JavidSumra <112365664+JavidSumra@users.noreply.github.com>
2023-07-14 17:17:28 +0530

    Update todo.js

--- Task-2/todo.js
+++ Task-2/todo.js
@@ -14,53 +14,62 @@ const getListofTodos = () => {
   // console.log(localStorage.length);
   if (localStorage.length != undefined) {
     for (let todo in localStorage) {
-      let item = JSON.parse(localStorage.getItem(todo))
+      let item = JSON.parse(localStorage.getItem(todo));
       if (localStorage.getItem(todo)) {
-        let date = new Date(
-          item?.dueDate
-        ).getTime();
+        let date = new Date(item?.dueDate).getTime();
         let today = new Date(new Date().toISOString().split("T")[0]).getTime();
+        console.log(date + "\n" + today);
         let li = document.createElement("li");
-        if (date < today && item?.isCompleted) {
+        if (date < today && !item?.isCompleted) {
           li.setAttribute("class", "OVERDUE");
           li.innerHTML =
-            `<input type='checkbox' ${item?.isCompleted?'checked':''} onClick='updateTodo(${item?.id})'>`+item?.todo+ " | " + item?.dueDate +
-            `<button class="del" style='margin-left:10px;' onclick='deleteTodo(${
-              item?.id
-            })'>Delete</button>`;
+            `<input type='checkbox' ${
+              item?.isCompleted ? "checked" : ""
+            } onClick='updateTodo(${item?.id})'>` +
+            item?.todo +
+            " | " +
+            item?.dueDate +
+            `<button class="del" style='margin-left:10px;' onclick='deleteTodo(${item?.id})'>Delete</button>`;
           overdueItem.appendChild(li);
           overdueCount.innerHTML = document.querySelectorAll(".OVERDUE").length;
-        } else if (date > today && item?.isCompleted ) {
+        } else if (date > today && !item?.isCompleted) {
           li.setAttribute("class", "DUELATER");
           // console.log(j);
           li.innerHTML =
-            `<input type='checkbox' ${item?.isCompleted?'checked':''}onClick='updateTodo(${item?.id})'>`+item?.todo + " | " + item?.dueDate +
-            `<button class="del" style='margin-left:10px;' onclick='deleteTodo(${
-              item?.id
-            })'>Delete</button>`;
+            `<input type='checkbox' ${
+              item?.isCompleted ? "checked" : ""
+            }onClick='updateTodo(${item?.id})'>` +
+            item?.todo +
+            " | " +
+            item?.dueDate +
+            `<button class="del" style='margin-left:10px;' onclick='deleteTodo(${item?.id})'>Delete</button>`;
           dueLaterItem.appendChild(li);
           duelaterCount.innerHTML =
             document.querySelectorAll(".DUELATER").length;
-        }
-        else if(item?.isCompleted){
+        } else if (item?.isCompleted) {
           li.setAttribute("class", "COMPLETED");
           li.innerHTML =
-            `<input type='checkbox' ${item?.isCompleted?'checked':''} onClick='updateTodo(${item?.id})'>`+item?.todo + " | " + item?.dueDate +
-            `<button class="del" style='margin-left:10px;' onclick='deleteTodo(${
-              item?.id
-            })'>Delete</button>`;
+            `<input type='checkbox' ${
+              item?.isCompleted ? "checked" : ""
+            } onClick='updateTodo(${item?.id})'>` +
+            item?.todo +
+            " | " +
+            item?.dueDate +
+            `<button class="del" style='margin-left:10px;' onclick='deleteTodo(${item?.id})'>Delete</button>`;
           completedItem.appendChild(li);
           completedCount.innerHTML =
             document.querySelectorAll(".COMPLETED").length;
-        }
-        else {
+        } else {
           li.setAttribute("class", "DUETODAY");
           // console.log(j);
           li.innerHTML =
-            `<input type='checkbox' ${item?.isCompleted?'checked':''}onClick='updateTodo(${item?.id})'>`+item?.todo + " | " + item?.dueDate +
-            `<button class="del" style='margin-left:10px;' onclick='deleteTodo(${
-              item?.id
-            })'>Delete</button>`;
+            `<input type='checkbox' ${
+              item?.isCompleted ? "checked" : ""
+            }onClick='updateTodo(${item?.id})'>` +
+            item?.todo +
+            " | " +
+            item?.dueDate +
+            `<button class="del" style='margin-left:10px;' onclick='deleteTodo(${item?.id})'>Delete</button>`;
           dueTodayItem.appendChild(li);
           duetodayCount.innerHTML =
             document.querySelectorAll(".DUETODAY").length;
@@ -75,12 +84,12 @@ const submit = () => {
   let dueDate = document.getElementById("dueDate");
   if (todoTitle.value.length != 0 && dueDate.value.length != 0) {
     let todoObj = {
-      id:i,
-      todo:todoTitle.value,
-      dueDate:dueDate.value,
-      isCompleted:false
-    }
-    localStorage.setItem(`detail-${i}`,JSON.stringify(todoObj));
+      id: i,
+      todo: todoTitle.value,
+      dueDate: dueDate.value,
+      isCompleted: false,
+    };
+    localStorage.setItem(`detail-${i}`, JSON.stringify(todoObj));
   }
   i += 1;
   // getListofTodos()
@@ -95,10 +104,10 @@ function deleteTodo(item) {
   // getListofTodos();
   window.location.reload();
 }
-const updateTodo = (id) =>{
-  let todo = JSON.parse(localStorage.getItem(`detail-${id}`))
+const updateTodo = (id) => {
+  let todo = JSON.parse(localStorage.getItem(`detail-${id}`));
   todo.isCompleted = !todo.isCompleted;
-  localStorage.removeItem(`detail-${id}`)
-  localStorage.setItem(`detail-${id}`,JSON.stringify(todo));
+  localStorage.removeItem(`detail-${id}`);
+  localStorage.setItem(`detail-${id}`, JSON.stringify(todo));
   window.location.reload();
-}
+};
